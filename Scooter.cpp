457375d3242da545a91de76919808487afe4688f
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-10-01 17:07:00 -0400

    Implemented Scooter Milage
    Implemented scooter (custom powered vehicle) milage estimation. Based on eScooter behavior in major cities.

--- Scooter.cpp
+++ Scooter.cpp
@@ -16,6 +16,20 @@ string Scooter::toString() {
     return "-> Scooter\n" + PoweredVehicle::toString();
 }
 
-double Scooter::mileageEstimate(double time) {
-    return time;
+double Scooter::mileageEstimate(double min) {
+    int milage;
+
+    // Random Milage Generation (10-18 MPM)
+    // (eScooter speed typically between 10 and 20 MPH ~= 1.3-1.5 MPM)
+    srand (time(NULL));
+    double randomMilage = ((rand() % 3 + 3)/10.0)+1;
+
+    // Travel Time Calculations
+    // 1 in 3 Chance of Speed Reduction Area (Will Reduce Milage by 50%)
+    milage = randomMilage * min;
+    if(rand()%3 == 1) {
+        milage -= milage * .5;
+    }
+
+    return milage;
 }
