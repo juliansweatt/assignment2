e665138b83aa0781fac9678c697dfb0ea917ff54
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-10-01 16:34:42 -0400

    Implemented Skateboard Milage Estimate
    Implemented milage estimates for the Skateboard Vehicle.

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -1,4 +1,6 @@
 #include "Skateboard.h"
+#include <cstdlib>
+#include <ctime>
 
 Skateboard::Skateboard() {
     setBrand("Custom");
@@ -12,8 +14,24 @@ Skateboard::Skateboard(string brand, string model) {
 
 Skateboard::~Skateboard() = default;
 
-double Skateboard::mileageEstimate(double time) {
-    return time;
+double Skateboard::mileageEstimate(double min) {
+    double milage;
+
+    // Random Milage Generation (0.1-0.5 MPM)
+    srand (time(NULL));
+    double randMilage = (rand() % 5 + 1)/10.0;
+
+    // Travel Time Calculations
+    milage = (randMilage) * min;
+
+    // Note, this condition was unclear in the instructions. Choose to place
+    // condition on the parameter time.
+    if(min > 25 && min < 250) {
+        double randTimeAdded = rand() % (int)(((1.0/3.0) * min) + 1);
+        milage += milage + randTimeAdded;
+    }
+
+    return (int)milage;
 }
 
 string Skateboard::toString() {
